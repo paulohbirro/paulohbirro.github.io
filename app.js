ec0fad35497b4b6ebb7f16a7104b59ffc1c884b2
Paulo Birro <33101308+paulohbirro@users.noreply.github.com>
2024-08-10 15:22:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-// Aplicando máscara de moeda brasileira (R$) que aparece conforme o usuário digita
-Inputmask({
-    alias: "currency",
-    prefix: "R$ ",
-    groupSeparator: ".",
-    radixPoint: ",",
-    digits: 2,
-    autoUnmask: true,
-    removeMaskOnSubmit: true,
-    autoGroup: true,
-    rightAlign: false
-}).mask(document.querySelectorAll("input"));
+    // Aplicando máscara de moeda brasileira (R$) que aparece conforme o usuário digita
+    Inputmask({
+        alias: "numeric",
+        groupSeparator: ".",
+        radixPoint: ",",
+        digits: 2,
+        prefix: "R$ ",
+        autoGroup: true,
+        rightAlign: false,
+        unmaskAsNumber: true,
+        removeMaskOnSubmit: true
+    }).mask(document.querySelectorAll("input"));
 
-document.getElementById('fuelForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    document.getElementById('fuelForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    let precoGasolina = parseFloat(document.getElementById('gasolina').inputmask.unmaskedvalue().replace(",", "."));
-    let precoAlcool = parseFloat(document.getElementById('alcool').inputmask.unmaskedvalue().replace(",", "."));
+        let precoGasolina = parseFloat(document.getElementById('gasolina').inputmask.unmaskedvalue());
+        let precoAlcool = parseFloat(document.getElementById('alcool').inputmask.unmaskedvalue());
 
-    // Cálculo da proporção
-    let proporcao = precoAlcool / precoGasolina;
+        // Cálculo da proporção
+        let proporcao = precoAlcool / precoGasolina;
 
-    // Verificação
-    let resultado = document.getElementById('resultado');
-    if (proporcao < 0.7) {
-        resultado.textContent = "É mais vantajoso abastecer com Álcool.";
-        resultado.classList.remove("text-danger");
-        resultado.classList.add("text-success");
-    } else {
-        resultado.textContent = "É mais vantajoso abastecer com Gasolina.";
-        resultado.classList.remove("text-success");
-        resultado.classList.add("text-danger");
-    }
-});
+        // Verificação
+        let resultado = document.getElementById('resultado');
+        if (proporcao < 0.7) {
+            resultado.textContent = "É mais vantajoso abastecer com Álcool.";
+            resultado.classList.remove("text-danger");
+            resultado.classList.add("text-success");
+        } else {
+            resultado.textContent = "É mais vantajoso abastecer com Gasolina.";
+            resultado.classList.remove("text-success");
+            resultado.classList.add("text-danger");
+        }
+    });
